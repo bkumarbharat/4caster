bf24dfc53ab78e3009aeb93d735c5c71a412075d
bkumarbharat <bkumarbharat0@gmail.com>
2021-05-31 15:33:43 -0400

    initial spa code

---
+++ 4caster-spa/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {Button} from '@material-ui/core';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button>Hello world!</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
